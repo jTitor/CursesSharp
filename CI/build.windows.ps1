326b2f412c81f8168a9b51030debe19ef086ad3e
jTitor <me@me.me>
2017-09-24 02:20:40 -0700

    build.windows.ps1: Add comments for TODO

--- CI/build.windows.ps1
+++ CI/build.windows.ps1
@@ -1,5 +1,25 @@
 #Cleans the local repo, then builds the native and CLI libraries.
 
+#TODO:
+#	* Check tools exist:
+#		* dotnet
+#		* dotnet is 2.0 or above so SLN to msbuild works
+#		* NMake exists (Cl.exe will likely be there if NMake is too)
+#	* If not, fail here
+#	* Check that PDCurses is in ./pdcurses:
+#		* pdcurses.lib
+#		* curses.h
+#		* panel.h
+#		* term.h
+#	* If not:
+#		* Clone PDCurses Github
+#		* Build pdcurses:
+#			```
+#			cd (PDCurses folder)\win32
+#			nmake -f vcwin32.mak WIDE=Y
+#			```
+#		* Copy .lib and headers to ./pdcurses
+
 # Clean native assemblies
 &dotnet msbuild CursesSharp.Native.sln /t:Clean /p:Configuration=Release
 &dotnet msbuild CursesSharp.Native.sln /t:Clean /p:Configuration=Debug
