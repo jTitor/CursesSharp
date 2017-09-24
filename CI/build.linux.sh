c9ee0e9e6c44aee3fc4c1616464440957859fcc3
jTitor <me@me.me>
2017-09-24 02:25:23 -0700

    build.linux.sh: Add comments

--- CI/build.linux.sh
+++ CI/build.linux.sh
@@ -1,2 +1,19 @@
 #~/bin/bash
 echo TODO
+
+#Note to self: this is not a provisioning script,
+#if any dependencies are missing it should fail
+#rather than try to install them
+#TODO:
+#	* Check dependencies exist
+#		* dotnet
+#			* dotnet >= 2.0
+#			* If missing, fail here
+#		* curses available (how?)
+#			* If not, fail here
+#	* Native
+#		* Clean: dotnet msbuild?
+#		* Build: dotnet msbuild?
+#	* CLI
+#		* Clean: dotnet msbuild?
+#		* Build: dotnet msbuild?
